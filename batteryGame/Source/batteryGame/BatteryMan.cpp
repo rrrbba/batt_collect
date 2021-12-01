d104436b056eff8f018616db4c3642f71de5c9f4
Rayven <rayven.burns@gmail.com>
2021-12-01 13:16:46 -0500

    initialized variables for character movement

--- batteryGame/Source/batteryGame/BatteryMan.cpp
+++ batteryGame/Source/batteryGame/BatteryMan.cpp
@@ -9,6 +9,22 @@ ABatteryMan::ABatteryMan()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//initialize variables
+	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
+
+	//Only allow camera to rotate, not character
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	//Allow character to rotate in the direction that it is moving
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	//The rate at which it rotates
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+	//The velocity when character jumps
+	GetCharacterMovement()->JumpZVelocity = 600.0f;
+	//How much you can control character in the air
+	GetCharacterMovement()->AirControl = 0.2f;
 }
 
 // Called when the game starts or when spawned
