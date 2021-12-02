2f65969b13d29943677d266121282654cc9cad48
Rayven <rayven.burns@gmail.com>
2021-12-02 16:48:09 -0500

    fixed the camera

---
+++ batteryGame/Intermediate/Build/Win64/batteryGameEditor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30133
+#include "C:/Users/Rayven Wang/Documents/GitHub/batt_collect/batteryGame/Intermediate/Build/Win64/batteryGameEditor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.h"
---
+++ batteryGame/Intermediate/Build/Win64/batteryGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30133
+#include "C:/Users/Rayven Wang/Documents/GitHub/batt_collect/batteryGame/Intermediate/Build/Win64/batteryGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
